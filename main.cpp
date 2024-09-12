38660c39bfed06e95abaa041b1d29564503263b3
Perry9124 <124435956+Perry9124@users.noreply.github.com>
2024-09-12 19:33:26 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include ".\game\Game.h"
+#include ".\players\NovicePlayer.h"
+#include <iostream>
+#include <windows.h>
+using namespace std;
+int main() {
+	Game myRPG;
+	myRPG.run();
+	return 0;
+}
